223d29fe67a2f9be157a08320872df088202a929
ozkr <oscarmenacho23@gmail.com>
2022-01-10 17:36:45 -0500

    Add ScrollReveal

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const navMenu = document.querySelector('.nav-menu');
 menuBtn.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
